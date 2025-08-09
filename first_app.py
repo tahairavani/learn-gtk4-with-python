6bc05adbf6f442dc5c15a8260fb928edb1b8d114
TahaIr88 <taha.iravani1@gmail.com>
2025-08-09 21:11:26 +0330

    create first app

---
+++ first_app.py
@@ -0,0 +1,31 @@
+import gi
+import sys
+
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+from gi.repository import Gtk, Adw
+
+
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.box1 = Gtk.Box(orientation = Gtk.Orientation.VERTICAL)
+        self.set_child(self.box1)
+        self.button = Gtk.Button(label="click me!")
+        self.box1.append(self.button)
+
+
+class MyApp(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate', self.on_activate)
+        
+    def on_activate(self, app):
+        self.win = MainWindow(application=app)
+        self.win.present()
+
+app = MyApp()
+app.run(sys.argv)
+
+
