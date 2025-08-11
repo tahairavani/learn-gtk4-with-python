f9efc37f6812ff213cf24e283828a49ac40a976c
TahaIr88 <taha.iravani1@gmail.com>
2025-08-11 13:01:24 +0330

    recode first_app.py

--- first_app.py
+++ first_app.py
@@ -1,31 +1,16 @@
 import gi
-import sys
 
 
 gi.require_version('Gtk', '4.0')
-gi.require_version('Adw', '1')
-from gi.repository import Gtk, Adw
+from gi.repository import Gtk
+
+def on_open_application(app):
+    window = Gtk.ApplicationWindow(application = app)
+    window.present()
 
 
-class MainWindow(Gtk.ApplicationWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.box1 = Gtk.Box(orientation = Gtk.Orientation.VERTICAL)
-        self.set_child(self.box1)
-        self.button = Gtk.Button(label="click me!")
-        self.box1.append(self.button)
-
-
-class MyApp(Adw.Application):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.connect('activate', self.on_activate)
-        
-    def on_activate(self, app):
-        self.win = MainWindow(application=app)
-        self.win.present()
-
-app = MyApp()
-app.run(sys.argv)
+app = Gtk.Application()
+app.connect("activate",on_open_application)
+app.run(None)
 
 
